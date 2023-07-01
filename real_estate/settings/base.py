902e64db04697126cc7ce3b35f638ffc7dc33053
jamal <abdijumale2@gmail.com>
2023-07-01 16:29:31 +0900

    added userprofile

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -133,6 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.User"
 
 import logging
 import logging.config
